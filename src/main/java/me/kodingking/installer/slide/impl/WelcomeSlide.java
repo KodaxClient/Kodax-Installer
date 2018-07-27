b3d65867df3acddb6c67957a1eb7bceb84c82d41
Koding King <djitechdude@gmail.com>
2018-07-27 18:34:42 +1000

    Tried to redo some stuff.

---
+++ src/main/java/me/kodingking/installer/slide/impl/WelcomeSlide.java
@@ -0,0 +1,32 @@
+package me.kodingking.installer.slide.impl;
+
+import me.kodingking.installer.InstallerMain;
+import me.kodingking.installer.slide.AbstractSlide;
+import me.kodingking.installer.utils.SwingUtil;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class WelcomeSlide extends AbstractSlide {
+
+    @Override
+    public void initialize(JFrame frame) {
+        try {
+            JLabel icon = new JLabel();
+            icon.setIcon(new ImageIcon(ImageIO.read(getClass().getResourceAsStream("/icon/xenon_logo_cropped.png")).getScaledInstance(242, 89, Image.SCALE_DEFAULT)));
+            icon.setBounds((InstallerMain.width - 242) / 2, 40, 242, 89);
+            frame.add(icon);
+
+            JButton continueButton = new JButton("Install Now");
+            continueButton.setBounds((InstallerMain.width - 120) / 2, 160, 130, 50);
+            continueButton.addActionListener(e -> InstallerMain.advance());
+            SwingUtil.decorate(continueButton);
+
+            frame.add(continueButton);
+        } catch (IOException ignored) {
+        }
+    }
+
+}
